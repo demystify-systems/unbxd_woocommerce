a6b25f6ab083ce1c0a1ab8b8ddbb44c68686cf99
shriharishastry <shriharishastrysmg@gmail.com>
2020-03-07 14:30:03 +0530

    add : installtion scripts

--- readme.txt
+++ readme.txt
@@ -1,9 +1,76 @@
-=== Plugin Name ===
-Contributors: (this should be a list of wordpress.org userid's)
-Tags: comments, spam
-Requires at least: 4.0.1
-Tested up to: 4.3
-Requires PHP: 5.6
-Stable tag: 4.3
-License: GPLv3 or later License
-URI: http://www.gnu.org/licenses/gpl-3.0.html
+=== Woocommerce  Unbxd PIM Product Infomation Management ===
+Contributors: Unbxd
+Tags: woocommerce, unbxd, pim, products, information
+Requires at least: 4.6
+Tested up to: 4.7
+Stable tag: 2.1.1
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Unbxd PIM - Get your product content in front of your shoppers faster
+
+== Description ==
+
+**Drive your business goals with Unbxd PIM:**
+
+*Get product content in front of customers faster*
+
+Create tasks to update product data, and assign them to members in your organization
+
+*Improve conversion rates*
+
+Create product experiences powered by rich and informative content that improve conversions
+
+*Facilitate easier cross-team collaboration*
+
+Simplify task delegation, secure sensitive information, and manage your product team as a cohesive unit
+
+*Enhance the overall quality of product content*
+
+Eliminate content errors and ensure consistency across your content management process
+
+
+**Customer Support**
+
+.
+
+== Installation ==
+
+Requires WooCommerce 2.6.0 or higher
+
+*From your WordPress dashboard (recommended)*
+
+1. Go to Plugins > Add New
+2. Search for: Unbxd PIM
+3. Click on Install Now to install and activate the plugin
+
+*From WordPress.org*
+
+1. Download the latest version zip file
+2. Go to Plugins > Add New
+3. Click on Upload Plugin to upload the zip file
+
+*To activate the plugin*
+
+
+== Frequently Asked Questions ==
+
+= How to create new consumer key and secret in WooCommerce? =
+
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.0 =
+* First Version of the plugin.
+
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+First Version
--- unbxd_woocommerce.php
+++ unbxd_woocommerce.php
@@ -1,26 +1,48 @@
 <?php
-   /*
-   Plugin Name: Unbxd PIM Woocommerce
-   Plugin URI: https://unbxd.com
-   description: >-
-      Plugin to integrate Unbxd PIM 
-   Version: 1.0
-   Author: Unbxd
-   Author URI: http://pim.unbxd.io/
-   License: GPL2
-   */
+/*
+Plugin Name: Unbxd PIM Woocommerce
+Plugin URI: https://unbxd.com
+description: >-
+   Plugin to integrate Unbxd PIM
+Version: 1.0
+Author: Unbxd
+Author URI: http://pim.unbxd.io/
+License: GPL2
+*/
 
 
-   /**
- * Displays a list of posts ordered by popularity
- *
- * Shows a simple list of post titles ordered by their view count
- *
- * @param integer $post_count The number of posts to show
- *
- */
-function add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function = '', $position = null ) {
-    return add_submenu_page( 'options-general.php', $page_title, $menu_title, $capability, $menu_slug, $function, $position );
+add_action('admin_menu','unbxd_pim_acions');
+
+
+function unbxd_pim_acions(){
+    wp_enqueue_script( 'woo_plugin_script', plugins_url('/index.js', __FILE__), array('jquery'), '1.0', true );
+    add_menu_page(
+        'UNBXD Woocommerce Plugin','Unbxd','manage_options','woocommerceunbxd','woocommerceplugin_admin', plugins_url('images/logo_mini.png',__FILE__),2
+    );
 }
 
+
+
+function woocommerceplugin_admin(){
+    $site_url = get_site_url();
+
+    ?>
+    <link rel="stylesheet" href="https://pim-static.unbxd.com/prod/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://pim-static.unbxd.com/prod/1.13.6/css/bundle.css">
+
+    <div class="wrap">
+
+        <div class="unit marginBottom alnCenter">
+            <div class="logo">
+                <img src="<?php echo plugins_url('images/-logo.png',__FILE__); ?>">
+            </div>
+        </div>
+
+        <div class="unit marginBottom alnCenter">
+            <h2>Congrats! You just installed Unbxd PIM Plugin successfully!</h2>
+        </div>
+        <?php echo $site_url; ?>"
+    </div>
+    <?php
+}
 ?>
